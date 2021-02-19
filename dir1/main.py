4719528920b6e0946cbfb8708c0eacae8cd65c65
taeho9o <1taeho9o@gmail.com>
2021-02-19 09:32:15 +0900

    2/19 dir test2

--- dir1/main.py
+++ dir1/main.py
@@ -1,5 +1,8 @@
 import cal_add
+import testmod
+
 
 if __name__ == '__main__':
     num = cal_add.add(5, 10)
     print(num)
+    testmod.print_mess()
---
+++ dir1/testmod.py
@@ -0,0 +1,2 @@
+def print_mess():
+    print('hello')
